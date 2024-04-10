37cb899b683244e437bb1b5a15860094e0dc7217
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-04-10 15:08:57 -0300

    FEAT: tooltip with angular material & icons with font awesome, infos on card

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip'
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [MatTooltipModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
@@ -11,10 +12,13 @@ export class CardComponent {
 
   @Input() card: any
   source: string
+  tooltip: string
 
   ngOnInit() {
     //substituir a string pelo valor do header correto
     this.source = `../../assets/img/1.png`
+
+    this.tooltip = `Esse vendedor é um vendedor nivel ${this.card.badge}`
   }
 
 }
