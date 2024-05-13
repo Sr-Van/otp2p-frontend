5ff26a292760fe9e25355830c2810334197aeeeb
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-05-13 10:11:58 -0300

    FEAT: paginator

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { SalesService } from '../../shared/services/sales.service';
-import { Subscription, filter } from 'rxjs';
+
 import { CardComponent } from '../../shared/components/card/card.component';
+
 import { Anuncio } from '../../shared/interfaces/arrays';
+
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { merge, of } from 'rxjs';
+import { startWith, switchMap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, MatPaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -15,13 +21,15 @@ export class HomeComponent {
   
   salesService = inject(SalesService)
 
-  servidores: any [] = ['red','blue','green','yellow','orange','black','white','purple','pink']
-  tipos: any [] = ['pokemon', 'hd', 'tm', 'item']
+  servidores: string [] = ['red','blue','green','yellow','orange','black','white','purple','pink']
+  tipos: string [] = ['pokemon', 'hd', 'tm', 'item']
   sales: Anuncio[] = []
   filteredSales: Anuncio[]
   
   subs: Subscription
   isload: boolean = false
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor() {
     this.subs = this.salesService.getAllSales().subscribe(data =>
@@ -64,4 +72,17 @@ export class HomeComponent {
       this.isload = true
     }, time);
   }
+
+  linkListToPaginator() {
+    merge(this.paginator.page).pipe(
+        startWith({}),
+        switchMap(() => {
+           return of(this.sales);
+    }))
+    .subscribe(res => {
+        const from = this.paginator.pageIndex * 10;
+        const to = from + 10;
+        this.filteredSales = res.slice(from, to);
+    });
+    }
 }
