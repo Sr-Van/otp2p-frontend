9a9488435837f372bc766f831c5c4c0aedd71de7
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-06-03 13:48:00 -0300

    FEAT: lazy load all routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,41 @@
-import { FaqContentComponent } from './shared/components/faq-content/faq-content.component';
 import { Routes } from '@angular/router';
-
-import { HomeComponent } from './pages/home/home.component';
-import { NewOfferComponent } from './pages/new-offer/new-offer.component';
-import { ItemComponent } from './pages/item/item.component';
-import { PlayerComponent } from './pages/player/player.component';
-import { NotLoggedComponent } from './pages/not-logged/not-logged.component';
-import { LoginComponent } from './pages/login/login.component';
 
 import { AuthGuard } from './shared/guard/auth.guard';
 import { LoginGuard } from './shared/guard/login.guard';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { TradesComponent } from './pages/trades/trades.component';
-import { FaqPageComponent } from './pages/faq-page/faq-page.component';
-import path from 'path';
+
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'add/offer', component: NewOfferComponent, canActivate: [AuthGuard]},
-    {path: 'item/:item', component: ItemComponent},
-    {path: 'player/:player', component: PlayerComponent},
-    {path: 'not-logged', component: NotLoggedComponent, canActivate: [LoginGuard]},
-    {path: 'login', component: LoginComponent, canActivate: [LoginGuard]},
-    {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
-    {path: 'trades', component: TradesComponent, canActivate: [AuthGuard]},
+    {path: '', 
+        loadComponent: () => import('./pages/home/home.component')
+        .then(c => c.HomeComponent)},
+    {path: 'add/offer', 
+        loadComponent: () => import('./pages/new-offer/new-offer.component')
+        .then(c => c.NewOfferComponent), canActivate: [AuthGuard]},
+    {path: 'item/:item', 
+        loadComponent: () => import('./pages/item/item.component')
+        .then(c => c.ItemComponent)},
+    {path: 'player/:player', 
+        loadComponent: () => import('./pages/player/player.component')
+        .then(c => c.PlayerComponent)},
+    {path: 'not-logged', 
+        loadComponent: () => import('./pages/not-logged/not-logged.component')
+        .then(c => c.NotLoggedComponent), canActivate: [LoginGuard]},
+    {path: 'login', 
+        loadComponent: () => import('./pages/login/login.component')
+        .then(c => c.LoginComponent), canActivate: [LoginGuard]},
+    {path: 'profile', 
+        loadComponent: () => import('./pages/profile/profile.component')
+        .then(c => c.ProfileComponent), 
+        canActivate: [AuthGuard]},
+    {path: 'trades', 
+        loadComponent: () => import('./pages/trades/trades.component')
+        .then(c => c.TradesComponent), 
+        canActivate: [AuthGuard]},
     {path: 'faq', 
-        loadComponent: () => import('./pages/faq-page/faq-page.component').then(c => c.FaqPageComponent), children :[
-            {path: 'filter/:filterParam', loadComponent: () => import('./shared/components/faq-content/faq-content.component').then(c => c.FaqContentComponent)}
+        loadComponent: () => import('./pages/faq-page/faq-page.component')
+        .then(c => c.FaqPageComponent), children :[
+            {path: 'filter/:filterParam', loadComponent: () => import('./shared/components/faq-content/faq-content.component')
+            .then(c => c.FaqContentComponent)}
         ]
     }
 ];
