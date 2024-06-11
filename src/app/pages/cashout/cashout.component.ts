3c4b9a4607a5c3f330842982b3624887cf7d23ca
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-06-11 16:29:55 -0300

    FEAT: filter only number on cashout form. STYLE: using same style as deposit page

--- src/app/pages/cashout/cashout.component.ts
+++ src/app/pages/cashout/cashout.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cashout',
   standalone: true,
-  imports: [LoadingSpinnerComponent],
+  imports: [LoadingSpinnerComponent, ReactiveFormsModule],
   templateUrl: './cashout.component.html',
-  styleUrl: './cashout.component.css'
+  styleUrl: '../deposit/deposit.component.css'
 })
 export class CashoutComponent {
 
+  cashoutForm: FormGroup
   isLoaded: boolean = false
+  cashoutPrice: string = ''
   constructor() {
     
+    this.cashoutForm = new FormGroup({
+      price: new FormControl
+    })
     this.loadContent(1000)
   }
 
@@ -22,5 +28,31 @@ export class CashoutComponent {
     setTimeout(() => {
       this.isLoaded = true
     }, time);
+  }
+
+  filterNumber(number: any, event: any) {
+    const isNumber = /^[0-9]$/i.test(event.key)
+
+    if(event.key === '.' && number.length > 1 && !this.cashoutPrice.includes('.')) {
+      this.cashoutPrice = number
+      return
+    }
+
+    if(event.key === 'Backspace') {
+      this.cashoutPrice = number
+      return
+    }
+    if(!isNumber) {
+      event.target.value = this.cashoutPrice
+      return
+    }
+
+    this.cashoutPrice = number
+
+  }
+
+  cashout() {
+
+    console.log(this.cashoutForm.value)
   }
 }
