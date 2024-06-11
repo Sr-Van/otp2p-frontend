f427595dfd6ec0ed69dcf9f98079ec0af5eec453
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-06-11 14:02:33 -0300

    FEAT: filter only number os deposit value add deposit route on menu and loading. STYLE: page styles update

--- src/app/pages/deposit/deposit.component.ts
+++ src/app/pages/deposit/deposit.component.ts
@@ -1,27 +1,61 @@
 import { Component } from '@angular/core';
 import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-deposit',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, LoadingSpinnerComponent],
   templateUrl: './deposit.component.html',
   styleUrl: './deposit.component.css'
 })
 export class DepositComponent {
 
   formDeposit: FormGroup
+  depositPrice: string = ''
+  isLoaded: boolean = false
 
   constructor() {
 
     this.formDeposit = new FormGroup({
       deposit: new FormControl
     })
+
+    this.loadContent(1000)
   }
 
   sendDeposit() {
 
+    //add here logic to insert deposit and deal with the response
     console.log(this.formDeposit.value)
+    this.formDeposit.reset()
   }
 
+  filterNumber(number: any, event: any) {
+    const isNumber = /^[0-9]$/i.test(event.key)
+
+    if(event.key === '.' && number.length > 1 && !this.depositPrice.includes('.')) {
+      this.depositPrice = number
+      return
+    }
+
+    if(event.key === 'Backspace') {
+      this.depositPrice = number
+      return
+    }
+    if(!isNumber) {
+      event.target.value = this.depositPrice
+      return
+    }
+
+    this.depositPrice = number
+
+  }
+
+  loadContent(time: number) {
+    this.isLoaded = false
+    setTimeout(() => {
+      this.isLoaded = true
+    }, time);
+  }
 }
