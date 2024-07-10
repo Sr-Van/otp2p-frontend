2b71845ad456778b3700a998085c304d812b76b7
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-07-10 15:09:42 -0300

    FEAT: feedback form & send method. styles: feedback forms styles

--- src/app/pages/feedback/feedback.component.ts
+++ src/app/pages/feedback/feedback.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginService } from '../../shared/services/login.service';
+import { UtilService } from '../../shared/services/util.service';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.css'
 })
 export class FeedbackComponent {
+  private formBuilder = inject(FormBuilder);
+  private login$ = inject(LoginService);
+  private utS$ = inject(UtilService);
 
+  public feedbackForm: FormGroup;
+
+  constructor() {
+    this.utS$.setPageName('Feedback - OTP2P');
+    this.feedbackForm = this.formBuilder.group({
+      name: [this.login$.playerName, [Validators.required]],
+      message: ['', [Validators.required]]
+    })
+  }
+
+  public sendFeedback() {
+    //adicionar integraçao ao backend depois
+    console.log(this.feedbackForm.value)
+  }
 }
